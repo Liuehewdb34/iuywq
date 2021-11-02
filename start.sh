30d719f82d34036bf4137dae8b9b095832b6e439
Liuehewdb34 <90996524+Liuehewdb34@users.noreply.github.com>
2021-11-02 07:37:40 +0000

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# configs
-AUUID=24b4b1e1-7a89-45f6-858c-242cf53b5bdb
-CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
+# configsAUUID=96918d2c-eba3-4efa-b69b-651e3723db89CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
 CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
 CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
